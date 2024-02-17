afce0187a4f3ecb481bd989e64763b2ed56fff0d
juanfvr <encjuanfr2006@gmail.com>
2024-02-17 15:32:14 -0400

    FrontEnd Card-component

--- obj/Debug/net8.0/conversao-temperatura.AssemblyInfo.cs
+++ obj/Debug/net8.0/conversao-temperatura.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("conversao-temperatura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fefdc11601a1153359480235255f946c5a59cd2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("conversao-temperatura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("conversao-temperatura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
